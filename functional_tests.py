3192f3aee84c0db8c798af644a03c5a9c0765568
Aloïs DELAUNE <alois.delaune@hotmail.fr>
2022-11-28 23:33:09 +0100

    rewrote functional_tests.py using unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,19 +1,29 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewViwitorTest(unittest.TestCase):
 
-# The user checks out the home page of a to-do app :
-browser.get('http://localhost:8000')
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
 
-# The user can see the page title and header mention to-do lsits
-assert 'To-Do' in browser.title
+    def tearDown(self) -> None:
+        self.browser.quit()
 
-# The user is invited to enter a to-do item
-# The user types "Buy vine for next date"
-# When user hits Enter the page updates and now lists "1: Buy vine for next date"
-# There still is a text box inviting user to type a nex item. The user enters "Buy cheese"
-# When user hits Enter the page updates and now lists both items
-# The app generates a URL related to the user's list
-# When visiting said URL the app shows the list
+    def test_can_start_list_and_retrieve_it(self):
+        # The user checks out the home page of a to-do app :
+        self.browser.get('http://localhost:8000') 
 
-browser.quit()
+        # The user can see the page title and header mention to-do lsits
+        self.assertIn('To-Do', self.browser.title)
+        self.fail("Finish the test!")
+
+        # The user is invited to enter a to-do item
+        # The user types "Buy vine for next date"
+        # When user hits Enter the page updates and now lists "1: Buy vine for next date"
+        # There still is a text box inviting user to type a nex item. The user enters "Buy cheese"
+        # When user hits Enter the page updates and now lists both items
+        # The app generates a URL related to the user's list
+        # When visiting said URL the app shows the list    
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
