8830f5e4baaac4931ea9b91986a90992007d77d8
Aloïs DELAUNE <alois.delaune@hotmail.fr>
2022-12-21 22:39:40 +0100

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -37,7 +37,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy vine for next date' for row in rows)
+            any(row.text == '1: Buy vine for next date' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # There still is a text box inviting user to type a nex item. The user enters "Buy cheese"
