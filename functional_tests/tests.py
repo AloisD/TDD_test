ac2ae2d1d21ee0bb20b76aa063f5d266c57f6022
Aloïs DELAUNE <alois.delaune@hotmail.fr>
2022-12-25 15:36:45 +0100

    Extended functional test for multiple users management

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -27,7 +27,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # The user checks out the home page of a to-do app :
         self.browser.get(self.live_server_url) 
 
@@ -59,7 +59,50 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy vine for next date')
         self.wait_for_row_in_list_table('2: Buy cheese')
         
-        # The app generates a URL related to the user's list
-        self.fail('Finish the test!')
+        # Satisfied, our first user goes back to sleep
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # User n°1 starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy vine for next date')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy vine for next date')
+
+        # User n°1 notices that his list has a unique URL
+        user_n1_list_url = self.browser.current_url
+        self.assertRegex(user_n1_list_url, '/lists/.+')
+
+        # User n°2 comes along
+
+        ## we need a new browser session to make sure that no information
+        ## of user n°1 is coming through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # User n°2 visits the home page. There is no sign of user n°1's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy vine for next date', page_text)
+        self.assertNotIn('Buy cheese', page_text)
+
+        # User n°2 starts a new listby entering a new item.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Iron my shirt')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Iron my shirt')
+
+        # User n°2 gets his own unique url
+        user_n2_list_url = self.browser.current_url
+        self.assertRegex(user_n2_list_url, '/lists/.+')
+        self.assertNotEqual(user_n2_list_url, user_n1_list_url)
+
+        # Again, no trace of user n°1's list here
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy vine for next date', page_text)
+        self.assertIn('Iron my shirt', page_text)
+
+        # Satisfied, both users go back to sleep
 
         # When visiting said URL the app shows the list    
+        self.fail('Finish the test!')
